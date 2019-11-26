7a8d232f04a651853ee919cacefec0e51db7440e
Maxim Koshman <maksimkoshman090@gmail.com>
2019-11-26 19:26:46 +0300

    add some js and modal windows

--- js/_custom.js
+++ js/_custom.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    // Slider
+
     $('#partners__slider').slick({
         arrows: false,
         slidesToShow: 4,
@@ -34,5 +36,78 @@ $(function() {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 5000,
+        responsive: [
+          {
+            breakpoint: 720,
+            settings: {
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2
+            }
+          }
+        ]
       });
+
+      // Burger
+
+      $("#nav__toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("show");
+        $("#nav").toggleClass("show");
+    });
+
+    // Inner HTML
+
+    let online = 5;
+    $("#online").html(online);
+
+    let money = 50000;
+    $("#money").html(money);
+
+    function addMoney() {
+      money += 2000;
+      $("#money").html(money);
+    }
+
+    setInterval(addMoney, 3000);
+
+    // Loading
+
+    function loadInfo() {
+      for (let i = 0; i < 10; i++) {
+        $('#load').animate({  borderSpacing: -360 }, {
+          step: function(now,fx) {
+            $(this).css('-webkit-transform','rotate('+now+'deg)'); 
+            $(this).css('-moz-transform','rotate('+now+'deg)');
+            $(this).css('transform','rotate('+now+'deg)');
+          },
+          duration:'slow'
+      },'linear');
+      };
+      
+      function changeInfo() {
+        $("#infoBody").css('display', 'block');
+        $("#load").css('display', 'none');
+      }
+
+      setTimeout(changeInfo, 3000);
+    }
+
+    loadInfo();
+
+
+    // for (let i = 1; i < 50; i++) {
+    //   loadInfo();
+    // }
+
+    // $('#load').animate({ transform: 'rotate(360deg)' }, { duration: 1000 });
+    // $('#load').animateRotate(90, 1337, 'linear', function () {});
+
+
+
 });
