ab2c14ef4fa8a9378c598c2799849ae020c8e842
Maxim Koshman <maksimkoshman090@gmail.com>
2019-11-27 10:29:45 +0300

    Relize Version

--- js/_custom.js
+++ js/_custom.js
@@ -1,150 +1,121 @@
 $(function() {
+  // Slider
 
-    // Slider
-
-    $('#partners__slider').slick({
-        arrows: false,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        responsive: [
-            {
-              breakpoint: 975,
-              settings: {
-                slidesToShow: 3
-              }
-            },
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 2
-              }
-              },
-            {
-              breakpoint: 410,
-              settings: {
-                slidesToShow: 1
-              }
-            }
-          ]
-      });
-
-      $('.social__inner').slick({
-        arrows: false,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        responsive: [
-          {
-            breakpoint: 720,
-            settings: {
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 2
-            }
-          }
-        ]
-      });
-
-      // Burger
-
-      $("#nav__toggle").on("click", function(event) {
-        event.preventDefault();
-
-        $(this).toggleClass("show");
-        $("#nav").toggleClass("show");
-    });
-
-    // Inner HTML
-
-    let online = 5;
-    $("#online").html(online);
-
-    let money = 50000;
-    $("#money").html(money);
-
-    function addMoney() {
-      money += 2000;
-      $("#money").html(money);
-    }
-
-    setInterval(addMoney, 3000);
-
-    // Loading
-
-    function loadInfo() {
-      for (let i = 0; i < 100; i++) {
-        $('#load').animate({  borderSpacing: -360 }, {
-          step: function(now,fx) {
-            $(this).css('-webkit-transform','rotate('+now+'deg)'); 
-            $(this).css('-moz-transform','rotate('+now+'deg)');
-            $(this).css('transform','rotate('+now+'deg)');
-          },
-          duration:'slow'
-      },'linear');
-      };
-
-      function changeInfo() {
-        $("#infoBody").css('display', 'block');
-        $("#load").css('display', 'none');
+  $(".social__inner").slick({
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2
+        }
       }
+    ]
+  });
 
+  // Burger
 
+  $("#nav__toggle").on("click", function(event) {
+    event.preventDefault();
 
-      setTimeout(changeInfo, 8000);
+    $(this).toggleClass("show");
+    $("#nav").toggleClass("show");
+  });
+
+  // Inner HTML
+
+  let online = 5;
+  $("#online").html(online);
+
+  let money = 50000;
+  $("#money").html(money);
+
+  function addMoney() {
+    money += 2000;
+    $("#money").html(money);
+  }
+
+  setInterval(addMoney, 3000);
+
+  // Loading
+
+  function loadInfo() {
+    for (let i = 0; i < 100; i++) {
+      $("#load").animate(
+        { borderSpacing: -360 },
+        {
+          step: function(now, fx) {
+            $(this).css("-webkit-transform", "rotate(" + now + "deg)");
+            $(this).css("-moz-transform", "rotate(" + now + "deg)");
+            $(this).css("transform", "rotate(" + now + "deg)");
+          },
+          duration: "slow"
+        },
+        "linear"
+      );
     }
 
-    loadInfo();
-
-    // Auth Btn
-
-    function heartBtn() {
-      $('#authBtn').addClass('animation');
+    function changeInfo() {
+      $("#infoBody").css("display", "block");
+      $("#load").css("display", "none");
     }
 
-    function remHearBtn() {
-      $('#authBtn').removeClass('animation');
-    }
+    setTimeout(changeInfo, 8000);
+  }
 
-    setInterval(remHearBtn, 100);
-    setInterval(heartBtn, 500);
+  loadInfo();
 
-    // Table
+  // Auth Btn
 
-    let date = new Date();
-    let year = date.getFullYear();
-    let month = date.getMonth();
-    let day = date.getDate();
-    let hour = date.getHours();
-    let minutes = date.getMinutes(); 
+  function heartBtn() {
+    $("#authBtn").addClass("animation");
+  }
 
-    $(".date").html(day + '.' + month + '.' + year + ' ' + hour + ':' + minutes);
+  function remHearBtn() {
+    $("#authBtn").removeClass("animation");
+  }
 
-    // function addPayItem() {
-    //   $('.payout__item:first-child').css('display', 'flex');
-    //   $('.payout__item:last-child').css('display', 'none');
-    // }
+  setInterval(remHearBtn, 100);
+  setInterval(heartBtn, 500);
 
-    // setInterval(addPayItem, 2000);
+  // Table
 
-     function addPayItem() {
-      $('.payout__wrapper:first-child').css('display', 'none');
-      $('.payout__wrapper:nth-child(2)').css('display', 'block');
-    }
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth();
+  let day = date.getDate();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
 
-    function addPayItem2() {
-      $('.payout__wrapper:nth-child(2)').css('display', 'none');
-      $('.payout__wrapper:last-child').css('display', 'block');
-    }
+  $(".date").html(day + "." + month + "." + year + " " + hour + ":" + minutes);
 
-    setTimeout(addPayItem, 3000);
-    setTimeout(addPayItem2, 6000);
+  // function addPayItem() {
+  //   $('.payout__item:first-child').css('display', 'flex');
+  //   $('.payout__item:last-child').css('display', 'none');
+  // }
 
+  // setInterval(addPayItem, 2000);
 
+  function addPayItem() {
+    $(".payout__wrapper:first-child").css("display", "none");
+    $(".payout__wrapper:nth-child(2)").css("display", "block");
+  }
+
+  function addPayItem2() {
+    $(".payout__wrapper:nth-child(2)").css("display", "none");
+    $(".payout__wrapper:last-child").css("display", "block");
+  }
+
+  setTimeout(addPayItem, 3000);
+  setTimeout(addPayItem2, 6000);
 });
